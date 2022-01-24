4d9e50433288eccce8f08dc4f7e042d8e26fd891
artouro <sir.louis.trenet@gmail.com>
2022-01-24 22:46:52 +0800

    Add perkiraan total populasi tanaman & hasil panen

--- app/Models/Kebun.php
+++ app/Models/Kebun.php
@@ -16,4 +16,14 @@ class Kebun extends Model
     public function jenis_tanaman(){
         return $this->hasOne(JenisTanaman::class, 'id', 'id_jenistanaman');
     }
+
+    public function getTotalPopulasiTanamanAttribute($value) {
+        $totalBibit = $this->luas_lahan / (($this->jarak_tanam / 100) ** 2);
+        return round($totalBibit);
+    }
+
+    public function getPerkiraanJumlahHasilPanenAttribute($value) {
+        $jumlahPanen = $this->total_populasi_tanaman * $this->harga_satuan_per_hasil_panen;
+        return $jumlahPanen;
+    }
 }
